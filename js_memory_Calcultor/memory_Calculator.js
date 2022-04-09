c7454ca204d2d2912dc0a7f813433cfd59429fe7
davebecoding <d01001010@csu.fullerton.edu>
2022-04-08 22:46:06 -0700

    Helper Files

--- js_memory_Calcultor/memory_Calculator.js
+++ js_memory_Calcultor/memory_Calculator.js
@@ -21,6 +21,7 @@ console.log(formatSize(example))
 // git add README.md
 // git commit -m "first commit"
 // git branch -M main
+//
 // git remote add origin git@github.com:DaveBeCoding/Helper_JS_Files.git
 // git push -u origin main
 // …or push an existing repository from the command line
