9fcd022cde5eb916f89acd4037db87b35c61b9a5
davebecoding <d01001010@csu.fullerton.edu>
2022-04-09 19:54:25 -0700

    Object :: For ... In :: Example

---
+++ Object_Iteration/Obj_Example_01.js
@@ -0,0 +1,39 @@
+/**
+ *          Adjective (MATHEMATICS) :: Enumerable ::
+ *      Able to be counted by one-to-one correspondence
+ *      with the set of all positive integers.
+ */
+
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
+  ssn: "123-456-7890",
+};
+
+console.log("\n")
+for (const key in person) {
+    if (person.hasOwnProperty(key)) {
+    console.log(`${key} : ${person[key]}`);
+  }
+}
+console.log("\n")
+
+//  NOTES
+
+/*
+        for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+
+        [Object.hasOwnProperty] :: method ensures that the property 
+        belongs to the person object, not the inherited properties.
+
+        The method will not be available in objects where it is reimplemented, 
+        or on objects created using Object.create(null) (as these don't inherit 
+        from Object.prototype). 
+
+*/
