2b9a319faf1e199182c2798507a7ebc4e0df7123
davebecoding <d01001010@csu.fullerton.edu>
2022-04-09 21:26:59 -0700

    Sample Output

--- Object_Iteration/Obj_Example_01.js
+++ Object_Iteration/Obj_Example_01.js
@@ -11,29 +11,39 @@ let person = {
   ssn: "123-456-7890",
 };
 
-console.log("\n")
+console.log("\n");
 for (const key in person) {
-    if (person.hasOwnProperty(key)) {
+  if (person.hasOwnProperty(key)) {
     console.log(`${key} : ${person[key]}`);
   }
 }
-console.log("\n")
+console.log("\n");
+
+// SAMPLE OUTPUT ->
+/*
+
+  firstName : John
+  lastName : Doe
+  age : 25
+  ssn : 123-456-7890
+
+ */
 
 //  NOTES
 
-/*
-        for (const key in object) {
-    if (Object.hasOwnProperty.call(object, key)) {
-        const element = object[key];
+
+//         for (const key in object) {
+//         if (Object.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
         
-    }
-}
+//     }
+// }
 
-        [Object.hasOwnProperty] :: method ensures that the property 
-        belongs to the person object, not the inherited properties.
+//         [Object.hasOwnProperty] :: method ensures that the property 
+//         belongs to the person object, not the inherited properties.
 
-        The method will not be available in objects where it is reimplemented, 
-        or on objects created using Object.create(null) (as these don't inherit 
-        from Object.prototype). 
+//         The method will not be available in objects where it is reimplemented, 
+//         or on objects created using Object.create(null) (as these don't inherit 
+//         from Object.prototype). 
 
-*/
+
