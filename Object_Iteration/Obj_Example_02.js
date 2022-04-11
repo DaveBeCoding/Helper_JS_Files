25036ace9d6f16858c6cb8062e9c4539c150c263
davebecoding <d01001010@csu.fullerton.edu>
2022-04-10 18:42:56 -0700

    enumerable propiies of object without executing on inherited properties

---
+++ Object_Iteration/Obj_Example_02.js
@@ -0,0 +1,14 @@
+let buz = {
+    fog: 'stack'
+  };
+  
+  for (let name in buz) {
+    if (buz.hasOwnProperty(name)) {
+      console.log('this is fog (' +
+        name + ') for sure. Value: ' + buz[name]);
+    }
+    else {
+      console.log(name); // toString or something else
+    }
+  }
+  
